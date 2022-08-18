4cf3d654c1cfa590f6537a4060cd4ebaf5f26127
Oleksii Verbytskyi <alverba88@gmail.com>
2022-08-18 14:10:18 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import styles from './App.module.css';
+
+export const App = () => {
+  return <>hello</>;
+};
